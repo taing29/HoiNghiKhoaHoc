aeae5a7820c2fc554edd91a4c4671ea103a63951
kanV5 <votaanlocc@gmail.com>
2025-05-17 03:39:49 +0700

    test merge

--- HoiNghiKhoaHoc/Program.cs
+++ HoiNghiKhoaHoc/Program.cs
@@ -22,7 +22,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Login}/{action=Index}/{id?}")
+    pattern: "{controller=Conferences}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
