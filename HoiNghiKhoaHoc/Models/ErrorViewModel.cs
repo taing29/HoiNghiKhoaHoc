5de2d71bfceda28c019dde16cd3262b60ea24e2f
taing <taifluf@gmail.com>
2025-05-17 00:34:25 +0700

    Add project files.

---
+++ HoiNghiKhoaHoc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HoiNghiKhoaHoc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
