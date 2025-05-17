14366e5c6e8741c48f87231f12445cbf9a9f0540
taing <taifluf@gmail.com>
2025-05-17 22:59:48 +0700

    2

--- HoiNghiKhoaHoc/Areas/Admin/Controllers/ConferencesController.cs
+++ HoiNghiKhoaHoc/Areas/Admin/Controllers/ConferencesController.cs
@@ -1,6 +1,8 @@
-﻿using HoiNghiKhoaHoc.Repositories;
+﻿using HoiNghiKhoaHoc.Models;
+using HoiNghiKhoaHoc.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HoiNghiKhoaHoc.Areas.Admin.Controllers
 {
@@ -21,5 +23,27 @@ namespace HoiNghiKhoaHoc.Areas.Admin.Controllers
             var refences = await _conferenceRepository.GetAllConferencesAsync();
             return View(refences);
         }
+        public async Task<IActionResult> Create()
+        {
+            var categories = await _categoryRepository.GetAllCategoriesAsync();
+            ViewBag.CategoryId = new SelectList(categories, "Id", "Name");
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Conference conference)
+        {
+            if (ModelState.IsValid)
+            {
+                conference.CreatedDate = DateTime.Now;
+                await _conferenceRepository.AddConferenceAsync(conference);
+                return RedirectToAction(nameof(Index));
+            }
+
+            var categories = await _categoryRepository.GetAllCategoriesAsync();
+            ViewBag.CategoryId = new SelectList(categories, "Id", "Name", conference.CategoryId);
+            return View(conference);
+        }
+
     }
 }
