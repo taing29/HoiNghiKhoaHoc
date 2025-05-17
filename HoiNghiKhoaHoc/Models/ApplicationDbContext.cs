a1d21e3ad20d57696b0ac36431b74b08fb290a02
taing <taifluf@gmail.com>
2025-05-17 14:46:50 +0700

    sua nhieu code

---
+++ HoiNghiKhoaHoc/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HoiNghiKhoaHoc.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        //khai báo các bảng trong cơ sở dữ liệu
+        public DbSet<Conference> Conferences { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- HoiNghiKhoaHoc/Program.cs
+++ HoiNghiKhoaHoc/Program.cs
@@ -1,7 +1,18 @@
+using HoiNghiKhoaHoc.Models;
+using HoiNghiKhoaHoc.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//các services cho các repository
+//builder.Services.AddScoped<IConferenceRepository, EFConferenceRepository>();
+//builder.Services.AddScoped<ICategoryRepository, EFCategoryRepository>();
 
 var app = builder.Build();
 
